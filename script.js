1aaa4a6432332cc57caac97eaa54686953984a1e
rachels-archive <rachelhui@protonmail.com>
2024-05-10 10:52:36 +0800

    Create html elements to display array as bars

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const n = 10;
-
-const array = [];
+const array = Array(10).fill(0);
+n = array.length;
 
 for (let i = 0; i < n; i++) {
   array[i] = Math.random();
 }
 
 console.log(array);
+
+for (let i = 0; i < n; i++) {
+  const bar = document.createElement("div");
+  bar.style.height = array[i] * 100 + "%";
+  bar.style.width = "10px";
+  bar.style.backgroundColor = "black";
+  const container = document.querySelector(".container");
+  container.appendChild(bar);
+}
