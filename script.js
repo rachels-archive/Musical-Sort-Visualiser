11c9c7b72dc519a51374f156b85338f2d11d648e
rachels-archive <rachelhui@protonmail.com>
2024-05-10 11:19:04 +0800

    Implement bar shuffling and sorting when buttons clicked

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 const array = Array(10).fill(0);
 n = array.length;
 
-for (let i = 0; i < n; i++) {
-  array[i] = Math.random();
+// initialise randomly generated bars
+initBars();
+
+function initBars() {
+  for (let i = 0; i < n; i++) {
+    array[i] = Math.random();
+  }
+  displayBars();
 }
 
-console.log(array);
-
-do {
-  var swapped = false;
-  for (let i = 1; i < n; i++) {
-    if (array[i - 1] > array[i]) {
-      [array[i - 1], array[i]] = [array[i], array[i - 1]];
-      swapped = true;
-    }
-  }
-} while (swapped);
-
-for (let i = 0; i < n; i++) {
-  const bar = document.createElement("div");
-  bar.className = "bar";
-  bar.style.height = array[i] * 100 + "%";
+function displayBars() {
   const container = document.getElementById("container");
-  container.appendChild(bar);
+  container.innerHTML = "";
+  for (let i = 0; i < n; i++) {
+    const bar = document.createElement("div");
+    bar.className = "bar";
+    bar.style.height = array[i] * 100 + "%";
+
+    container.appendChild(bar);
+  }
+}
+
+function bubbleSort() {
+  do {
+    var swapped = false;
+    for (let i = 1; i < n; i++) {
+      if (array[i - 1] > array[i]) {
+        [array[i - 1], array[i]] = [array[i], array[i - 1]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  displayBars();
 }
