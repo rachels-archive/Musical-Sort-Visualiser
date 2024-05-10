d00ab0cb309e367d6cfa86d889f13a0c2adc3c12
rachels-archive <rachelhui@protonmail.com>
2024-05-10 23:05:19 +0800

    Add animation for the swaps

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const array = Array(10).fill(0);
-n = array.length;
+const n = 10;
+const array = [];
 
 // initialise randomly generated bars
 initBars();
@@ -11,28 +11,60 @@ function initBars() {
   displayBars();
 }
 
-function displayBars() {
+function sort() {
+  /*
+    array.slice() sorts the copy of the global array.
+    By creating a copy of the array, animate() can modify the copy
+    without affecting the original array, ensuring accurate
+    representation of each step taken in the sorting process.
+   */
+  const swaps = bubbleSort(array.slice());
+  animate(swaps);
+}
+
+function animate(swaps) {
+  if (swaps.length == 0) {
+    displayBars();
+    return;
+  }
+
+  const [i, j] = swaps.shift();
+  [array[i], array[j]] = [array[j], array[i]];
+  displayBars([i, j]);
+
+  setTimeout(function () {
+    animate(swaps);
+  }, 100);
+}
+
+function bubbleSort(array) {
+  const swaps = [];
+  do {
+    var swapped = false;
+    for (let i = 1; i < array.length; i++) {
+      if (array[i - 1] > array[i]) {
+        // keep track of the swaps for animation
+        swaps.push([i - 1, i]);
+        swapped = true;
+        // swapping array element
+        [array[i - 1], array[i]] = [array[i], array[i - 1]];
+      }
+    }
+  } while (swapped);
+  return swaps;
+}
+
+function displayBars(indices) {
   const container = document.getElementById("container");
   container.innerHTML = "";
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < array.length; i++) {
     const bar = document.createElement("div");
     bar.className = "bar";
     bar.style.height = array[i] * 100 + "%";
 
+    if (indices && indices.includes(i)) {
+      bar.style.backgroundColor = "#f00";
+    }
     container.appendChild(bar);
   }
-}
-
-function bubbleSort() {
-  do {
-    var swapped = false;
-    for (let i = 1; i < n; i++) {
-      if (array[i - 1] > array[i]) {
-        [array[i - 1], array[i]] = [array[i], array[i - 1]];
-        swapped = true;
-      }
-    }
-  } while (swapped);
-
-  displayBars();
 }
