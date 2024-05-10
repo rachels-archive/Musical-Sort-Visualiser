83dbe0fbc5c8b5c842b0388fbaca50f4fee4488c
rachels-archive <rachelhui@protonmail.com>
2024-05-10 11:09:31 +0800

    Implement bubble sort

--- script.js
+++ script.js
@@ -7,11 +7,20 @@ for (let i = 0; i < n; i++) {
 
 console.log(array);
 
+do {
+  var swapped = false;
+  for (let i = 1; i < n; i++) {
+    if (array[i - 1] > array[i]) {
+      [array[i - 1], array[i]] = [array[i], array[i - 1]];
+      swapped = true;
+    }
+  }
+} while (swapped);
+
 for (let i = 0; i < n; i++) {
   const bar = document.createElement("div");
+  bar.className = "bar";
   bar.style.height = array[i] * 100 + "%";
-  bar.style.width = "10px";
-  bar.style.backgroundColor = "black";
   const container = document.getElementById("container");
   container.appendChild(bar);
 }
