f42bac7cd203081cc5d4361eca4d8085175e6734
rachels-archive <rachelhui@protonmail.com>
2024-05-10 23:58:39 +0800

    Bug fix: Pause the sort after shuffle button clicked

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-const n = 10;
+const n = 30;
 const array = [];
+
+let sortTimeout = null;
 
 // initialise randomly generated bars
 initBars();
 
+let audioContext = null; // audio context
+
+function playNote(freq) {
+  if (audioContext == null) {
+    audioContext = new (AudioContext || webkitAudioContext || window.webkitAudioContext)();
+  }
+  const duration = 0.1;
+  const osc = audioContext.createOscillator();
+  osc.frequency.value = freq;
+  osc.start();
+  osc.stop(audioContext.currentTime + duration);
+  const node = audioContext.createGain();
+  node.gain.value = 0.1;
+  node.gain.linearRampToValueAtTime(0, audioContext.currentTime + duration);
+  osc.connect(node);
+  node.connect(audioContext.destination);
+}
+
 function initBars() {
+  // Clear any existing sorting timeout
+  if (sortTimeout !== null) {
+    clearTimeout(sortTimeout);
+    sortTimeout = null;
+  }
+
   for (let i = 0; i < n; i++) {
     array[i] = Math.random();
   }
@@ -18,43 +44,54 @@ function sort() {
     without affecting the original array, ensuring accurate
     representation of each step taken in the sorting process.
    */
-  const swaps = bubbleSort(array.slice());
-  animate(swaps);
+  const moves = bubbleSort(array.slice());
+  animate(moves);
 }
 
-function animate(swaps) {
-  if (swaps.length == 0) {
+function animate(moves) {
+  if (moves.length == 0) {
     displayBars();
     return;
   }
 
-  const [i, j] = swaps.shift();
-  [array[i], array[j]] = [array[j], array[i]];
-  displayBars([i, j]);
+  const move = moves.shift();
+  const [i, j] = move.indices;
 
-  setTimeout(function () {
-    animate(swaps);
-  }, 100);
+  if (move.type == "swap") {
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  playNote(200 + array[i] * 500);
+  playNote(200 + array[j] * 500);
+  displayBars(move);
+
+  sortTimeout = setTimeout(function () {
+    animate(moves);
+  }, 10);
 }
 
 function bubbleSort(array) {
-  const swaps = [];
+  const moves = [];
+
   do {
     var swapped = false;
     for (let i = 1; i < array.length; i++) {
       if (array[i - 1] > array[i]) {
-        // keep track of the swaps for animation
-        swaps.push([i - 1, i]);
+        // keep track of the moves for animation
+        moves.push({
+          indices: [i - 1, i],
+          type: "swap",
+        });
         swapped = true;
         // swapping array element
         [array[i - 1], array[i]] = [array[i], array[i - 1]];
       }
     }
   } while (swapped);
-  return swaps;
+  return moves;
 }
 
-function displayBars(indices) {
+function displayBars(move) {
   const container = document.getElementById("container");
   container.innerHTML = "";
   for (let i = 0; i < array.length; i++) {
@@ -62,8 +99,8 @@ function displayBars(indices) {
     bar.className = "bar";
     bar.style.height = array[i] * 100 + "%";
 
-    if (indices && indices.includes(i)) {
-      bar.style.backgroundColor = "#f00";
+    if (move && move.indices.includes(i)) {
+      bar.style.backgroundColor = move.type == "swap" ? "#F00" : "#00F";
     }
     container.appendChild(bar);
   }
